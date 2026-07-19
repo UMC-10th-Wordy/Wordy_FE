9aa7e01b4568b42ed64f156c97cedc7f6c1e5d9c
aeongiing <ywjk0515@gmail.com>
2026-07-20 03:34:31 +0900

    fix: 스크롤바 상하 여백 통일 (pt-2 → py-2)

--- src/components/common/Layout/AppLayout.tsx
+++ src/components/common/Layout/AppLayout.tsx
@@ -4,7 +4,7 @@ import { Scrollbar } from '@/components/common/Scrollbar/Scrollbar'
 export function AppLayout() {
   return (
     <div className="h-screen flex flex-col overflow-hidden">
-      <Scrollbar scrollbarClassName="pt-2 pr-1">
+      <Scrollbar scrollbarClassName="py-2 pr-1">
         <Outlet />
       </Scrollbar>
     </div>
