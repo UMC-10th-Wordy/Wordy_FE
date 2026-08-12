92a1ebea918e3d2f3fb4f3e393305945f674be43
aeongiing <ywjk0515@gmail.com>
2026-08-12 15:50:41 +0900

    fix: 휴지통 일지 상세 페이지가 휴지통 전용 API를 사용하도록 수정
    - 일반 일지 상세 조회 API 호출로 인한 404 수정
    - 성과 패널 마운트 전 activeWorkspaceId 준비 여부 확인 가드 추가

--- src/pages/trash/TrashDiaryDetailPage.tsx
+++ src/pages/trash/TrashDiaryDetailPage.tsx
@@ -1,5 +1,27 @@
-import { DiaryDetailPage } from '@/pages/diary/DiaryDetailPage'
+import { useParams } from 'react-router-dom'
+
+import { LoadingState } from '@/components/common/AsyncState/AsyncState'
+import { DiaryDetailContent } from '@/pages/diary/DiaryDetailPage'
+import { useGetTrashDailyEntryDetail } from '@/hooks/useTrashQueries'
+import { useActiveWorkspaceId } from '@/hooks/useWorkspaceQueries'
+
+const TrashDiaryDetailFetcher = ({ diaryId }: { diaryId: string }) => {
+  const { data: diary } = useGetTrashDailyEntryDetail(diaryId)
+
+  return <DiaryDetailContent diary={diary} hideDelete />
+}
 
 export function TrashDiaryDetailPage() {
-  return <DiaryDetailPage hideDelete />
+  const { diaryId } = useParams<{ diaryId: string }>()
+  const activeWorkspaceId = useActiveWorkspaceId()
+
+  if (!diaryId) {
+    throw new Error('업무 일지 ID가 없습니다.')
+  }
+
+  if (!activeWorkspaceId) {
+    return <LoadingState message="불러오는 중입니다" className="h-full w-full" />
+  }
+
+  return <TrashDiaryDetailFetcher key={diaryId} diaryId={diaryId} />
 }
