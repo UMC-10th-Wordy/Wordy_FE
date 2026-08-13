f5810be814679c725a4597ba4aeddd923e6210a9
aeongiing <ywjk0515@gmail.com>
2026-08-13 16:56:45 +0900

    feat: 휴지통 진입 위치를 프로필 메뉴에서 히스토리 헤더로 이동
    업무 일지 히스토리 헤더에 휴지통 이동 버튼 추가, 프로필 메뉴의 휴지통 항목 제거

--- src/components/diary-list/DiaryListHeader.tsx
+++ src/components/diary-list/DiaryListHeader.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import { TextButton } from '@/components/common/Button/TextButton'
 import { SearchInput } from '@/components/common/SearchInput/SearchInput'
 import { useRecentSearchKeywords } from '@/hooks/useRecentSearchKeywords'
+
+import directionRightIcon from '@/assets/icons/Direction=right.svg'
 
 export const DiaryListHeader = () => {
   const navigate = useNavigate()
@@ -26,7 +29,7 @@ export const DiaryListHeader = () => {
   }
 
   return (
-    <header className="relative z-50 flex w-full items-start justify-between">
+    <header className="relative z-50 flex w-full items-center justify-between">
       <div className="flex shrink-0 flex-col">
         <h1 className="[font-size:var(--font-size-heading-4)] leading-(--line-height-body) font-[var(--font-weight-bold)] text-(--color-text-default)">
           업무 일지 히스토리
@@ -38,35 +41,46 @@ export const DiaryListHeader = () => {
         </p>
       </div>
 
-      <div
-        className="min-w-0 max-w-145 flex-1"
-        onMouseDownCapture={(event) => {
-          const target = event.target as HTMLElement
+      <div className="flex min-w-0 flex-1 items-center justify-end gap-3">
+        <TextButton
+          variant="text_neutral"
+          size="medium"
+          iconRight={<img src={directionRightIcon} alt="" aria-hidden="true" />}
+          onClick={() => navigate('/trash')}
+        >
+          휴지통
+        </TextButton>
 
-          if (target.closest('button')) {
-            event.preventDefault()
-          }
-        }}
-        onKeyDownCapture={(event) => {
-          if (event.key === 'Enter' && event.nativeEvent.isComposing) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-        }}
-      >
-        <SearchInput
-          value={searchKeyword}
-          onChange={(event) => setSearchKeyword(event.target.value)}
-          onFocus={() => setIsSearchFocused(true)}
-          onBlur={() => setIsSearchFocused(false)}
-          onSearch={handleSearch}
-          recentKeywords={recentKeywords}
-          onRemoveKeyword={removeRecentKeyword}
-          onClearAll={clearRecentKeywords}
-          placeholder={isSearchFocused ? '' : '업무 내용 또는 키워드를 검색해 보세요'}
-          aria-label="업무 일지 검색"
-          className="w-full [&>div:nth-child(2)]:top-[calc(100%+12px)] [&>div:nth-child(2)]:left-1/2 [&>div:nth-child(2)]:z-50 [&>div:nth-child(2)]:-translate-x-1/2"
-        />
+        <div
+          className="min-w-0 max-w-145 flex-1"
+          onMouseDownCapture={(event) => {
+            const target = event.target as HTMLElement
+
+            if (target.closest('button')) {
+              event.preventDefault()
+            }
+          }}
+          onKeyDownCapture={(event) => {
+            if (event.key === 'Enter' && event.nativeEvent.isComposing) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+          }}
+        >
+          <SearchInput
+            value={searchKeyword}
+            onChange={(event) => setSearchKeyword(event.target.value)}
+            onFocus={() => setIsSearchFocused(true)}
+            onBlur={() => setIsSearchFocused(false)}
+            onSearch={handleSearch}
+            recentKeywords={recentKeywords}
+            onRemoveKeyword={removeRecentKeyword}
+            onClearAll={clearRecentKeywords}
+            placeholder={isSearchFocused ? '' : '업무 내용 또는 키워드를 검색해 보세요'}
+            aria-label="업무 일지 검색"
+            className="w-full [&>div:nth-child(2)]:top-[calc(100%+12px)] [&>div:nth-child(2)]:left-1/2 [&>div:nth-child(2)]:z-50 [&>div:nth-child(2)]:-translate-x-1/2"
+          />
+        </div>
       </div>
     </header>
   )
